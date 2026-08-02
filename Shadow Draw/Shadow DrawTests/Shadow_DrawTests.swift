579ee5e237aef395e658139855c31509553b13a3
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 21:54:51 -0400

    Add Shadow Draw/Shadow DrawTests/Shadow_DrawTests.swift

---
+++ Shadow Draw/Shadow DrawTests/Shadow_DrawTests.swift
@@ -0,0 +1,17 @@
+//
+//  Shadow_DrawTests.swift
+//  Shadow DrawTests
+//
+//  Created by Elliot Williams on 2025-07-27.
+//
+
+import Testing
+@testable import Shadow_Draw
+
+struct Shadow_DrawTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
